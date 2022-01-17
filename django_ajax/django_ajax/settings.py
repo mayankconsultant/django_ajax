ea3ae5ef5cdab04ceb23f57d768dc4837e2f7e11
Mayank Shah <mayank.shah@ss.zain.com>
2022-01-17 21:04:51 +0200

    Initial_bf_users_af_secret_remove using django environ

--- django_ajax/django_ajax/settings.py
+++ django_ajax/django_ajax/settings.py
@@ -13,6 +13,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import  os
 
+import environ
+
+env = environ.Env()
+
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i08b2$$u5us2j^975k&3y)+u@%nd7d3!r52(vzt4n%!^2ewk@%'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
